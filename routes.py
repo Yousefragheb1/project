e19491c5ccd4d85d9a36772d221b797f6872ec10
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2023-07-25 13:56:59 +1200

    setup file structure and database with sample data

--- routes.py
+++ routes.py
@@ -3,27 +3,42 @@ import sqlite3
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route ("/footy")
-def footy():
-    conn= sqlite3.connect("database.db")
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM Lesson")
-    results = cur.fetchall()
-    return render_template(""football.html" , results=results , title = FOOTBALL PAGE ")
+@app.route('/passing')
+def passing():
+    return render_template('passing.html')
 
-@app.route('/footy/<int:id>')
-def footy(id):
+@app.route('/shooting')
+def shooting():
+    return render_template('shooting.html')
+
+@app.route('/dribbling')
+def dribbling():
+    return render_template('dribbling.html')
+
+
+@app.route("/course")
+def course():
+    conn = sqlite3.connect("database.db")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM course ")
+    results = cur.fetchall()
+    return render_template("course.html", results=results, title="course")
+
+@app.route('/football/<int:id>')
+def football(id):
     conn = sqlite3.connect('database.db')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM lesson WHERE id=?',(id,))
-    lesson = cur.fetchone()
-    cur.execute('SELECT name FROM course WHERE id=?',(id,))
-    course = cur.fetchone()
-    return render_template('footy.html' , lesson=lesson , course=course)
+    cur.execute('SELECT * FROM course WHERE id=?', (id,))
+    football = cur.fetchone()
+    print(football)
+    cur.execute('SELECT name FROM course WHERE id=?', (football[2],))
+    review = cur.fetchone()
+    return render_template('football.html', football=football, review=review)
+
 
 if __name__ == "__main__":
- app.run(debug=True)
+    app.run(debug=True)
